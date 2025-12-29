bf97b2a26d5b16a45e7296bf195597a2e5e40be4
ADALBERT JUNIOR TAKOTUE MABOU <maboujunior777@gmail.com>
2025-12-29 01:12:56 +0100

    Refactor: force_init.js to execute init_fixed.sql as a single query

--- scripts/force_init.js
+++ scripts/force_init.js
@@ -16,31 +16,8 @@ async function forceInit() {
         const schemaPath = path.join(__dirname, '..', 'database_correction', 'init_fixed.sql');
         const schemaSqlRaw = fs.readFileSync(schemaPath, 'utf8');
         
-        const norm = schemaSqlRaw
-            .replace(/\uFEFF/g, '')
-            .replace(/\r\n/g, '\n')
-            .replace(/\/\*[\s\S]*?\*\//g, ''); // remove /* */ blocks
-        const statements = norm
-            .split('\n')
-            .filter(line => !/^\s*--/.test(line))
-            .join('\n')
-            .split(';')
-            .map(s => s.trim())
-            .filter(s => /\S/.test(s))
-            .filter(s => /^[A-Za-z]/.test(s));
-            
-        console.log('Executing', statements.length, 'statements from init_fixed.sql');
-        
-        await client.query('BEGIN');
-        for (const stmt of statements) {
-            try {
-                await client.query(stmt);
-            } catch (e) {
-                console.error('Statement failed:', stmt.slice(0, 120) + '...', e.message);
-                throw e;
-            }
-        }
-        await client.query('COMMIT');
+        // Execute the entire SQL script as one query
+        await client.query(schemaSqlRaw);
         
         console.log('Database schema initialized successfully.');
     } catch (e) {
